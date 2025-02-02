25df809a3cb6269a7e23eb41fe4f56fe9fef4443
ARO-stark <oar31032003@gmail.com>
2025-02-02 20:18:56 +0100

    Update test_code.c

--- test_code.c
+++ test_code.c
@@ -4,55 +4,194 @@
 #include <math.h>
 #include "ARO.h"
 
-int nb;
-int resultat = bigramme("nbnb","nb");
-    printf("Le bigramme apparait %d fois.\n", resultat);
-int main() {
+
+
+
+double a, b, c, x1, x2;
+
+
+
+int main(){
+
+
+
+    printf("RESOLUTION DE L\'EQUATION SECOND aX^2 + bX + c");
+
+
+
+    printf("\n Veuillez entrer la valeur du coefficient a :");
+
+    scanf("%lf", &a);
+
+    printf("\n");
+
+
+
+    printf("\n Veuillez entrer la valeur du coefficient b :");
+
+    scanf("%lf", &b);
+
+    printf("\n");
+
+
+
+    printf("\n Veuillez entrer la valeur du coefficient c :");
+
+    scanf("%lf", &c);
+
+    printf("\n");
+
+
+
+    void racine(a, b, c);
+
+
+
+    char chaine, bi;
+
+    printf("Veuillez entrer une expression ");
+
+    scanf("%s", chaine);
+
+    printf("Veuillez entrer le bigramme ");
+
+    scanf("%s", bi);
+
+    bigramme(bi, chaine);
+
+
+
+
+
+
+
+    int nb;
+
     printf("Veuillez entrer un nombre a trois chiffres");
+
     scanf("%d", &nb);
-    
+
+
+
     // Conversion entier -> chaîne
+
     char buffer[5];
+
     sprintf(buffer, "%d", nb);
-    
-    
+
+
+
+
+
     // Conversion chaîne -> entier
+
     char c[] = "123";
-    int nb = atoi(c);
+
+    int nb0 = atoi(c);
+
     printf("Entier : %d\n", nb);
 
-   
-    
+
+
+
+
     int N = strlen(buffer);
+
     printf("Chaîne : %s\n", buffer);
-    
+
+
+
     char c1 = '3'; // Représente le chiffre 3
+
     char c2 = '5'; // Représente le chiffre 5
 
+
+
     // Convertir les caractères en leurs valeurs numériques
+
     int nb1 = c1 - '0'; // 3
+
     int nb11 = atoi(c1);//3
+
     int nb2 = c2 - '0'; // 5
-    int nb22 = atoi(c2); 
+
+    int nb22 = atoi(c2);
+
+
 
     int sum = nb1 + nb2 ; // 3 + 5 = 8
 
+
+
     printf("Somme des chiffres : %d\n", sum);
 
-    return 0;
-}
 
-    double a, b, c;
-    printf("Entrez les trois longueurs des côtés du triangle : ");
-    scanf("%lf %lf %lf", &a, &b, &c);
 
-    if (triangle(a, b, c)) {
-        printf("Le triangle est rectangle.\n");
+    double a0, b0, c0;
+
+
+
+    printf("\n Entrez les trois longueurs des cotes du triangle : ");
+
+    printf("\n Entrez la première longueur des cotes du triangle : ");
+
+    scanf("%lf", a0);
+
+    printf("\n Entrez la deuxième longueur des cotes du triangle : ");
+
+    scanf("%lf", b0);
+
+    printf("\n Entrez la deuxième longueur des cotes du triangle : ");
+
+    scanf("%lf", c0);
+
+
+
+
+
+    if (triangle(a0, b0, c0)) {
+
+        printf("\n Le triangle est rectangle.");
+
     } else {
-        printf("Le triangle n'est pas rectangle.\n");
+
+        printf("\n Le triangle n'est pas rectangle.\n");
+
     }
 
- int a, b;
+
+
+    float as, bs;
+
     printf("Entrez deux entiers : ");
-    scanf("%d %d", &a, &b);
-    printf("Le minimum est : %d\n", minimum(a, b));
+
+    scanf("%f %f", &as, &bs);
+
+    printf("Le minimum est : %d \n", minimum(a, b));
+
+
+
+    int nb3;
+
+    printf("\t\t \t |' ' ' MENU ' ' '| \n");
+
+    printf("\t\t 1. ENREGISTRER LES ENSEIGNANTS. \t\t |\n");
+
+    printf("\t\t 2. RECHERCHE UN ENSEIGNANTS.\t\t | \n");
+
+    printf("\t\t 3. AFFICHER LES ENSEIGNANTS.\t\t | \n");
+
+    printf("\t\t 4. MODIFIER LES INFOS D'UN ENSEIGNANT. \t\t | \n");
+
+    printf("\t\t 5. SUPPRIMER UN ENSEIGNANT. \t\t | \n");
+
+    printf("\t\t 6. TRIER LA LISTE DES ENSEIGNANTS. \t\t | \n");
+
+
+
+
+
+    return 0;
+
+}
+
